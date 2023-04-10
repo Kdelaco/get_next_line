a7e19cb3f9037e0a101f8b3102b8a077505085a6
Kevin De La Colina Kalinowski <kde-la-c@c1r12s2.42madrid.com>
2023-04-10 19:17:33 +0200

    splitting functions

--- get_next_line.c
+++ get_next_line.c
@@ -30,45 +30,62 @@ char	*get_start(char *buff, int *n)
 	return (ret);
 }
 
+char	*read_line(char *buff, int fd, int *n)
+{
+	char		*ret;
+	char		*tmp;
+	ssize_t		bsize;
+
+	ret = NULL;
+	bsize = 1;
+	while (!*n && bsize > 0)
+		if (!*buff)
+			bsize = read(fd, buff, BUFFER_SIZE);
+	else
+	{
+		tmp = get_start(buff, n);
+		if (!ret)
+			ret = ft_strdup(tmp);
+		else
+			ret = ft_strjoin(ret, tmp);
+		if (!*n)
+			ft_bzero(buff, BUFFER_SIZE + 1);
+	}
+	if (*n == 1)
+		free(tmp);
+	if (bsize == -1)
+		*n = bsize;
+	return (ret);
+}
+
 char	*get_next_line(int fd)
 {
 	char		*ret;
 	char		*tmp;
 	char		buff[BUFFER_SIZE + 1];
 	static char	cachito[BUFFER_SIZE];
-	size_t		bsize;
 	int			n;
 
-	if (BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	if (BUFFER_SIZE <= 0)
 		return (NULL);
 	ft_bzero(buff, BUFFER_SIZE + 1);
 	ret = NULL;
-	bsize = 1;
 	n = 0;
 	if (cachito[0])
 	{
 		ft_memmove(buff, cachito, ft_strlen(cachito));
 		ft_bzero(cachito, ft_strlen(cachito));
 	}
-	while (!n && bsize > 0)
-	{
-		if (!*buff)
-			bsize = read(fd, buff, BUFFER_SIZE);
-		if (bsize > 0)
-		{
-			tmp = get_start(buff, &n);
-			if (!ret)
-				ret = ft_strdup(tmp);
-			else
-				ret = ft_strjoin(ret, tmp);
-			if (!n)
-				ft_bzero(buff, BUFFER_SIZE + 1);
-		}
-	}
+	ret = read_line(buff, fd, &n);
 	if (n == 1)
 	{
-		free(tmp);
-		ft_memmove(cachito, ft_strchr(buff, '\n') + 1, BUFFER_SIZE);
+		tmp = ft_strchr(buff, '\n') + 1;
+		ft_memmove(cachito, tmp, ft_strlen(tmp));
+	}
+	if (n == -1)
+	{
+		free(ret);
+		return (0);
 	}
 	return (ret);
 }
@@ -79,7 +96,7 @@ char	*get_next_line(int fd)
 	int		i;
 	char	*str;
 
-	fd = open("tests/variable_nls.txt", O_RDONLY);
+	fd = open("tests/test2", O_RDONLY);
 	i = 0;
 	if (!(fd > -1))
 	{
@@ -96,6 +113,7 @@ char	*get_next_line(int fd)
 		str = get_next_line(fd);
 		printf("%s", str);
 		free(str);
+		// i++;
 	}
 
 	// system("leaks a.out");
